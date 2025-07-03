c415e143ac64eb577bebe0a093037be58a98c23e
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-07-03 10:10:43 -0500

    Update main_script21.R

--- main_script21.R
+++ main_script21.R
@@ -194,7 +194,7 @@ results_out <- data.frame(
 header <- '"number of obstacles" "length" "cost" "number of disambiguations"'
 
 # Define output path
-txt_path <- file.path(output_dir, "result_AP_obstacle.txt")
+txt_path <- file.path(output_dir, "result_DT_obstacle.txt")
 
 # Write header manually
 writeLines(header, txt_path)
